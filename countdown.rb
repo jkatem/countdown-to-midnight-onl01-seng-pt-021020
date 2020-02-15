d3d0ceadf2bb35e2985bf6f5efd202528797f7db
Jen Miao <jenmiao@gmail.com>
2020-02-15 00:08:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,19 @@ def countdown(num)
     puts "#{num} SECOND(S)!"
     num -= 1
   end
-  'HPPY NEW YEAR!'
+  'HAPPY NEW YEAR!'
 end
 countdown(10)
+
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+
+    sleep(1)
+  end
+  'HAPPY NEW YEAR!'
+end
+countdown(10)
+
+  
